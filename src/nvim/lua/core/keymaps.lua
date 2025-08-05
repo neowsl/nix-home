24614fb39628604d60eda979d8bc6919d0592ddb
Neal Wang <nealwang.sh@protonmail.com>
2025-08-04 18:25:01 -0700

    feat: remove nvim buffers

--- src/nvim/lua/core/keymaps.lua
+++ src/nvim/lua/core/keymaps.lua
@@ -3,8 +3,8 @@ vim.keymap.set("n", "<Leader><Left>", "<C-w>h")
 vim.keymap.set("n", "<Leader><Down>", "<C-w>j")
 vim.keymap.set("n", "<Leader><Right>", "<C-w>l")
 
-vim.keymap.set("n", "<Leader>j", ":bprevious<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>k", ":bnext<CR>", { silent = true })
+-- vim.keymap.set("n", "<Leader>j", ":bprevious<CR>", { silent = true })
+-- vim.keymap.set("n", "<Leader>k", ":bnext<CR>", { silent = true })
 vim.keymap.set(
 	"n",
 	"<Leader>q",
