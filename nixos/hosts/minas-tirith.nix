213c586167ae9db0bffd43f2eb0df806672b7d28
Neal Wang <nealwang.sh@protonmail.com>
2026-08-01 17:37:10 +0000

    fix: additional openssh macs for cloudflare

--- nixos/hosts/minas-tirith.nix
+++ nixos/hosts/minas-tirith.nix
@@ -78,6 +78,13 @@
         PasswordAuthentication = false;
         PermitRootLogin = "prohibit-password";
         TrustedUserCAKeys = "/etc/ssh/cloudflare-ca.pub";
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+          "hmac-sha2-512"
+          "hmac-sha2-256"
+        ];
       };
     };
     xserver = {
